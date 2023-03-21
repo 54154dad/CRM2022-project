9d22cc3c26e0462c857170b0b6d0491d40fe11c9
54154dad <2467883840@qq.com>
2023-03-21 15:42:17 +0800

    gitee commit

--- src/main/java/com/bjpowernode/crm/workbench/web/controller/activityController.java
+++ src/main/java/com/bjpowernode/crm/workbench/web/controller/activityController.java
@@ -394,6 +394,7 @@ public class activityController {
             System.out.println("hot-fix test!");
             System.out.println("push test!");
              System.out.println("pull test!");
+             System.out.println("gitee test!");
         }
     }
 
