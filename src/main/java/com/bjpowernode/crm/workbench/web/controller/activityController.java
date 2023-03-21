edd24148539cf4dda926dd906d60efd4bde29f19
YaoQ <102270298+54154dad@users.noreply.github.com>
2023-03-21 14:54:53 +0800

    Update activityController.java
    pull test

--- src/main/java/com/bjpowernode/crm/workbench/web/controller/activityController.java
+++ src/main/java/com/bjpowernode/crm/workbench/web/controller/activityController.java
@@ -393,6 +393,7 @@ public class activityController {
             System.out.println("master test");
             System.out.println("hot-fix test!");
             System.out.println("push test!");
+             System.out.println("pull test!");
         }
     }
 
